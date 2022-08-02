ecf114aaa77e8c9073e9e1b03fdd34b9f1d43dfc
silchyn <ivan.silchin@gmail.com>
2022-08-02 10:57:24 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# 2048 game
+Stack used:
+- HTML
+- SCSS(Sass)
+- JavaScript
+
+[DEMO](https://silchyn.github.io/2048-game)<br />
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
-'use strict';
+import { draw, spawn } from './field';
+import { started, resetGame, loseGame } from './game';
+import { moveDown, moveLeft, moveRight, moveUp } from './movement';
+
+const button = document.querySelector('.button');
+const messages = document.querySelectorAll('.message');
+const movementByKey = {
+  'ArrowDown': moveDown,
+  'ArrowLeft': moveLeft,
+  'ArrowRight': moveRight,
+  'ArrowUp': moveUp,
+};
+
+button.addEventListener('click', () => {
+  for (const message of messages) {
+    message.classList.add('hidden');
+  }
+
+  resetGame();
+  draw();
+});
+
+document.addEventListener('keydown', keyboardEvent => {
+  if (!started || !keyboardEvent.key.startsWith('Arrow')) {
+    return;
+  }
+
+  button.classList.remove('start');
+  button.classList.add('restart');
+  button.innerText = 'Restart';
+
+  if (movementByKey[keyboardEvent.key]()) {
+    spawn();
+  }
+
+  draw();
+  loseGame();
+});
